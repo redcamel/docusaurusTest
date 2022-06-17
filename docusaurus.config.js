4761435cae5d9f071189c7be70ce7247ba105c9f
webseon@gmail.com <dreamFActory3!>
2022-06-17 13:41:41 +0900

    - deploy test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'docusaurusTest', // Usually your repo name.
   // trailingSlash: false,
   presets: [
     [
