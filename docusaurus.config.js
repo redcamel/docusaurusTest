e5e2fd58545b6f9704713034e4d4e7490a291da0
webseon@gmail.com <dreamFActory3!>
2022-06-17 12:33:33 +0900

    - first test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
+  trailingSlash: false,
   presets: [
     [
       'classic',
